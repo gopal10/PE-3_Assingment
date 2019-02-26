2afc7d02a8624f8ff266c21ed501cd4b12646067
gopal10 <gopalpanchal10@gmail.com>
2019-02-26 15:01:46 +0530

    Update ChessPattern.java

--- src/main/java/com/stackroute/pe3/ChessPattern.java
+++ src/main/java/com/stackroute/pe3/ChessPattern.java
@@ -1,5 +1,5 @@
 package com.stackroute.pe3;
-
+//Write a program to create a ChessBoard pattern with the help of multidimensional array, where WWrepresents white color and BB represents Black color. Output: My Chess Board WW|BB|WW|BB|WW|BB|WW|BB| BB|WW|BB|WW|BB|WW|BB|WW| WW|BB|WW|BB|WW|BB|WW|BB| BB|WW|BB|WW|BB|WW|BB|WW| WW|BB|WW|BB|WW|BB|WW|BB| BB|WW|BB|WW|BB|WW|BB|WW| WW|BB|WW|BB|WW|BB|WW|BB| BB|WW|BB|WW|BB|WW|BB|WW|
 public class ChessPattern {
 
     public String [][]  chess(int rows,int cols)
